5e1aceea435bdf606ef8876eed05143fc7ff662f
Sambytes <79375141+SamRam360@users.noreply.github.com>
2022-01-13 17:08:54 -0600

    Add New Project
    First commit, adding new project :) This way i can work across multiple devices

---
+++ Yve of Dawn/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    PlayerControls playerControls;
+
+    public Vector2 movementInput;
+    public float verticalInput;
+    public float horizonalInput;
+
+    private void OnEnable()
+    {
+        if (playerControls == null)
+        {
+            playerControls = new PlayerControls();
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>();
+        }
+
+        playerControls.Enable();
+
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public void HandleAllInputs()
+    {
+        HandleMovementInput();
+        //HandleJumpingInput
+        //HandleActionInput
+    }
+
+    
+
+    private void HandleMovementInput()
+    {
+        verticalInput = movementInput.y;
+        horizonalInput = movementInput.x;
+    }
+}
