c9fd75ce69fa2dd7b397c7bcb54e50ac9af68de1
Sambytes <79375141+SamRam360@users.noreply.github.com>
2022-01-14 07:15:04 -0600

    Stairs and Slopes
    Adjusted height to capsule collider on player, to 1.6 - and had script memorize the last location of the Feet so that i can ramp up Stairs

--- Yve of Dawn/Assets/Scripts/Movement/PlayerLocomotion.cs
+++ Yve of Dawn/Assets/Scripts/Movement/PlayerLocomotion.cs
@@ -123,7 +123,9 @@ public class PlayerLocomotion : MonoBehaviour
     {
         RaycastHit hit;
         Vector3 rayCastOrigin = transform.position;
+        Vector3 targetPosition;
         rayCastOrigin.y = rayCastOrigin.y + rayCastHeightOffSet;
+        targetPosition = transform.position;
 
         if (!isGrounded && !isJumping)
         {
@@ -144,12 +146,27 @@ public class PlayerLocomotion : MonoBehaviour
                 animatorManager.PlayTargetAnimation("Land", true);
 
             }
+
+            Vector3 rayCastHitPoint = hit.point;
+            targetPosition.y = rayCastHitPoint.y;
             inAirTimer = 0;
             isGrounded = true;
         }
         else
         {
             isGrounded = false;
+        }
+
+        if (isGrounded && !isJumping)
+        {
+            if (playerManager.isInteracting || inputManager.moveAmount > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime / 0.1f);
+            }
+            else
+            {
+                transform.position = targetPosition;
+            }
         }
     }
 
